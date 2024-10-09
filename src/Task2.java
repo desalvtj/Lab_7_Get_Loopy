ff9c69b94dbcbe00f32a8b6abf113a7c0b655065
desalvtj <desalvtj@mail.uc.edu>
2024-10-09 17:00:08 -0400

    Task 2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,12 @@
-public class Task2 {
+public class Task2
+{
+
+    public static void main(String[] args)
+    {
+
+        for(int c = 30; c >= 0; c--)
+            System.out.print(" " + c);
+
+
+    }
 }
